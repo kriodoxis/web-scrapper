1e34ec3a1d5b989ad03d690954530bc47924aded
kriodoxis <mario_urquilla@unicomer.com>
2021-01-05 11:57:26 -0600

    Added multiprocessing library

--- bb-scrapper.py
+++ bb-scrapper.py
@@ -1,20 +1,22 @@
+import json
+import logging.config
 import os
+import time
+from re import sub
+import multiprocessing
 
-from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.chrome.options import Options
+import apprise
+from bs4 import BeautifulSoup
+from chromedriver_py import binary_path
 # Load selenium components
 from selenium import webdriver
-from chromedriver_py import binary_path
-from bs4 import BeautifulSoup
-import apprise
-import pandas as pd
-import time
-import logging.config
-from tld import get_fld
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support.ui import WebDriverWait
+from tld import get_fld
 
 logging.basicConfig(
     format='%(asctime)s - %(levelname)s - %(message)s',
@@ -33,16 +35,16 @@ chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 
 
-def url_parser(item_url):
+def url_parser(item_url, min, max):
     switcher = {
         'bestbuy.com': bestbuy,
         'amazon.com': amazon,
     }
-    parser = switcher.get(get_fld(item_url), lambda x: logger.info(f'Skipping {x}'))
-    parser(item_url)
+    parser = switcher.get(get_fld(item_url), lambda x, y, z: logger.info(f'Skipping {x}'))
+    parser(item_url, min, max)
 
 
-def bestbuy(item_link):
+def bestbuy(item_link, min, max):
     try:
         driver.get(item_link)
         # driver.implicitly_wait(5)
@@ -57,23 +59,24 @@ def bestbuy(item_link):
         title = item_soup.select("div.sku-title > h1")
         btn_text = btn[0].text.strip().lower()
         is_sold = "sold out" == btn_text or "coming soon" == btn_text
-        logger.info(f'Item: {title[0].text.strip()} Is Sold?: {is_sold}')
+        logger.info(f'Item from BestBuy: {title[0].text.strip()} Is Sold?: {is_sold}')
         if not is_sold:
             apobj.notify(
                 body=f'Result for {title[0].text.strip()}: {btn[0].text.strip()}',
                 title='RTX Notifier',
             )
     except:
-        logger.exception("Fatal error in Bestbuy parse")
+        logger.exception(f'Fatal error in Bestbuy parsing: {item_link}')
 
 
-def amazon(item_link):
+def amazon(item_link, min, max):
     try:
         driver.get(item_link)
         item_soup = BeautifulSoup(driver.page_source, 'lxml')
         availability = item_soup.select("div#availability > span.a-size-medium.a-color-price")
         title = item_soup.select("span#productTitle")
         is_sold = True
+        is_fair = False
         if len(availability) == 0:
             try:
                 l = driver.find_element_by_css_selector("div#availability > span.a-size-medium.a-color-success > span.a-declarative > a")
@@ -81,39 +84,52 @@ def amazon(item_link):
                 seller_prices = wait.until(ec.presence_of_element_located((By.CSS_SELECTOR, "div#aod-offer-list span.a-price span.a-offscreen")))
                 item_soup = BeautifulSoup(driver.page_source, 'lxml')
                 seller_prices = item_soup.select("div#aod-offer-list span.a-price span.a-offscreen")
-                logger.info(seller_prices)
+                fair_prices = [x.text for x in seller_prices if min < float(sub(r'[^\d\-.]', '', x.text)) < max]
+                is_fair = len(fair_prices) > 0
+            except TimeoutException:
+                logger.exception(f'Error waiting for: {item_link}')
             except:
-                logger.exception("error")
+                logger.exception(f'Error Parsing: {item_link}')
 
-        logger.info(f'Item: {title[0].text.strip()} Is Sold?: {is_sold}')
-        if not is_sold:
+        logger.info(f'Item from Amazon: {title[0].text.strip()} Is Sold?: {is_sold or not is_fair}')
+        if not is_sold and is_fair:
             apobj.notify(
                 body=f'Result for {title[0].text.strip()}: {availability}',
                 title='RTX Notifier',
             )
     except:
-        logger.exception("Fatal error in Amazon parse")
+        logger.exception(f'Fatal error in Amazon parsing: {item_link}')
 
 
-tracker_url = 'https://www.nowinstock.net/computers/videocards/nvidia/rtx3060ti/'
-logger.info("Starting")
+CONFIG_PATH = "config/app_config.json"
+logger.info("Starting Scrapper")
 driver = webdriver.Chrome(executable_path=binary_path, options=chrome_options)
 wait = WebDriverWait(driver, 10)
-while True:
-    driver.get(tracker_url)
-    page_source = driver.page_source
-    soup = BeautifulSoup(page_source, 'lxml')
-    links = soup.select("#trackerContent > #data tr[id] td > a:not(.help)")
-    for link in links:
-        try:
-            driver.get(link['href'])
-            url_parser(driver.current_url)
-        except:
-            logger.exception("Fatal error in main loop")
-            break
-    logger.info("Finished website scrapping")
-    time.sleep(180)
+# p = multiprocessing.Pool(multiprocessing.cpu_count())  # Pool tells how many at a time
+
+if os.path.exists(CONFIG_PATH):
+    with open(CONFIG_PATH) as json_file:
+        while True:
+            config = json.load(json_file)
+            for item in config["items"]:
+                logger.info(f"Parsing Item: {item['name']} From: {item['url']}")
+                driver.get(item["url"])
+                page_source = driver.page_source
+                soup = BeautifulSoup(page_source, 'lxml')
+                links = soup.select("#trackerContent > #data tr[id] td > a:not(.help)")
+                for link in links:
+                    try:
+                        driver.get(link['href'])
+                        url_parser(driver.current_url, float(item["min"]), float(item["max"]))
+                    except:
+                        logger.exception(f"Fatal error in main loop for: {link['href']}")
+                        break
+            logger.info("Finished website scrapping")
+            time.sleep(180)
+else:
+    logger.error("No config file found, see here on how to fix this: https://github.com/Hari-Nagarajan/fairgame/wiki/Usage#json-configuration")
+    driver.close()
+    driver.quit()
+    exit(0)
 
 
-driver.close()
-driver.quit()
